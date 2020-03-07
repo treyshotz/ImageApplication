14737ea6164f9deb262a8594efe6e14984ffd05c
Nicolay Schiøll-Johansen <nicolayschiolljohansen@gmail.com>
2020-03-07 01:36:03 +0100

    Added basic layout for login and sign up pages in the new project structure

--- src/main/java/NTNU/IDATT1002/App.java
+++ src/main/java/NTNU/IDATT1002/App.java
@@ -15,7 +15,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"));
+        scene = new Scene(loadFXML("Login"));
         stage.setScene(scene);
         stage.show();
     }
