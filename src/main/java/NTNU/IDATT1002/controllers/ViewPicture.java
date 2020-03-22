636696954fc9902a5fb0ca2d570ec0b2c7fe679b
Nicolay Schiøll-Johansen <nicolayschiolljohansen@gmail.com>
2020-03-22 14:51:30 +0100

    Created view picture page

---
+++ src/main/java/NTNU/IDATT1002/controllers/ViewPicture.java
@@ -0,0 +1,43 @@
+package NTNU.IDATT1002.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+
+public class ViewPicture {
+    public ImageView tbar_logo;
+    public TextField tbar_search;
+    public Button tbar_searchButton;
+    public Button tbar_explore;
+    public Button tbar_map;
+    public Button tbar_upload;
+    public ImageView picture;
+    public Text picture_tagsField;
+    public Text picture_title_field;
+    public Text desc_textField;
+    public Pane metadata_pane;
+
+    public void switchToSearch(ActionEvent actionEvent) {
+
+    }
+
+    public void switchToExplore(ActionEvent actionEvent) {
+
+    }
+
+    public void switchToMap(ActionEvent actionEvent) {
+
+    }
+
+    public void switchToUpload(ActionEvent actionEvent) {
+
+    }
+
+    public void openPopUpPicture(MouseEvent mouseEvent) {
+        //method that opens pop-up of picture
+    }
+}
